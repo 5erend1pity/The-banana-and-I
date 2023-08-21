00afd7258a69a0f8004d47b7af4ee81b50a1d334
Carl Erlano <carl@synovize.com>
2023-08-21 16:08:17 -0400

    Orange

--- orange.py
+++ orange.py
@@ -4,4 +4,5 @@ print(banana)
 Multi-line comment
 Git- A Version Control System that allows multiple people to work
 on the same project simultaneously without interfering with one another.
+The the cat threw the apple on the captain's head
 """

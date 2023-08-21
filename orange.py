4943062968605f9bc8885cf0978e467de4c47e9c
Carl Erlano <carl@synovize.com>
2023-08-21 13:31:11 -0400

    Orange

--- orange.py
+++ orange.py
@@ -1,3 +1,7 @@
-
 banana = "red"
 print(banana)
+""" 
+Multi-line comment
+Git- A Version Control System that allows multiple people to work
+on the same project simultaneously without interfering with one another.
+"""

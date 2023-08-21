d7c3b89c80a47356ec0b219cac8f5f9754535fe5
Carl Erlano <carl@synovize.com>
2023-08-21 16:27:11 -0400

    Orange

--- orange.py
+++ orange.py
@@ -4,5 +4,10 @@ print(banana)
 Multi-line comment
 Git- A Version Control System that allows multiple people to work
 on the same project simultaneously without interfering with one another.
-The the cat threw the apple on the captain's head
+That is so flexible that we can reverse it's previous state
+Git is divided into 2 category, centralized and distributed system
+Centralized: All team members connect to a single server to get the latest version of the code
+Distributed: Every team member has a copy of the project that can be shared directly to others/
+The cat threw the apple on the captain's head
+
 """
